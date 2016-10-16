f8c6d5c8e45124453df8a196e58f0a0fb4ff0c71
Kevin W <gra230434@gmail.com>
2016-10-16 20:12:38 +0800

    PC

--- Lab1/Lab1-1/pro_1.c
+++ Lab1/Lab1-1/pro_1.c
@@ -21,7 +21,7 @@ double waste_time(long n)
 {
     double res = 0;
     long i = 0;
-    while (i <n * 200000000) {
+    while (i <n * 100000000) {
         i++;
         res += sqrt(i);
     }
@@ -41,6 +41,25 @@ void *thread_func(void *param)
     printf("Core 0 is running!\n");
     /* waste some time so the work is visible with "top" */
 	  printf("result: %f\n", waste_time(5));
+
+    pthread_exit(NULL);
+
+}
+
+void *thread_func1(void *param)
+{
+    CPU_ZERO(&cpuset);
+    CPU_SET(1, &cpuset); /* cpu 0 is in cpuset now */
+
+    /* bind process to processor 0 */
+    if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) !=0) {
+        perror("pthread_setaffinity_np");
+    }
+
+    printf("Core 1 is running!\n");
+    /* waste some time so the work is visible with "top" */
+	  printf("result: %f\n", waste_time(5));
+
     pthread_exit(NULL);
 
 }
@@ -57,6 +76,11 @@ int main(int argc, char *argv[])
     }
     pthread_join(my_thread,NULL);
 
+    if (pthread_create(&my_thread, NULL, thread_func1, NULL) != 0)
+    {
+        perror("pthread_create");
+    }
+    pthread_join(my_thread,NULL);
     endwtime = time (NULL);
     printf ("wall clock time = %ld sec\n", (endwtime - startwtime));
     return 0;
